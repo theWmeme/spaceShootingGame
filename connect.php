fc7fd2c732db120c02bfcaa19ec7290f91610c83
DoodlyDoodlly <113187784+theWmeme@users.noreply.github.com>
2026-06-01 19:54:59 -0400

    website hosting working

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "db.fr-pari1.bengt.wasmernet.com:10272";
-$username = "user_c7dd8dab";
+$username = "user_795eccd1";
 $password = "pw_190e1852";
-$dbname = "db_02961433";
+$dbname = "spaceShooting";
 
 
 $conn = new mysqli($servername, $username, $password, $dbname);
