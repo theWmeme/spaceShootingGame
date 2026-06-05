964294f265c953522c9e3d6755eefa3fe725233c
DoodlyDoodlly <113187784+theWmeme@users.noreply.github.com>
2026-06-04 23:18:42 -0400

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "db.fr-pari1.bengt.wasmernet.com:10272";
-$username = "user_795eccd1";
-$password = "pw_190e1852";
-$dbname = "spaceShooting";
+$servername = "mysql.railway.internal";
+$username = "root";
+$password = "TkHfatdjMelXctSkthIVQbatoOCRXlGs";
+$dbname = "railway";
 
 
 $conn = new mysqli($servername, $username, $password, $dbname);
